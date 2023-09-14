c80cbe378fde9e5d8a8c7b1ae905090815ccedc5
dawittamene <dawittamene559@gmail.com>
2023-09-14 20:14:30 +0300

    add trmplates

--- song/views.py
+++ song/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render, HttpResponse
 
 def index(request):
-    return HttpResponse('hello, world')
+    return render(request, 'song/main.html')
+
+def pictures(request):
+    return render(request, 'song/pictures.html')
+def video(request):
+    return render(request,'song/video.html')
