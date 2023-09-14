7aced914a0a6e9c12762be6ea2c42b2fa36bdf0f
dawittamene <dawittamene559@gmail.com>
2023-09-14 23:23:53 +0300

    commit

--- song/urls.py
+++ song/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('signup/', views.signup, name="signup"),
     path('loginpage/', views.loginpage, name="loginpage"),
     path('logoutPage/', views.logoutPage, name="logoutPage"),
+    
 ]
