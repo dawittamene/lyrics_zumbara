89f7fb8e03c673a293c532cfa6e1fdb457ccf8f7
dawittamene <dawittamene559@gmail.com>
2023-09-15 20:52:18 +0300

    models

--- song/models.py
+++ song/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Video(models.Model):
     description=models.CharField(max_length=200)
+    name=models.CharField(max_length=200,unique=True, null=True)
     video=models.FileField(upload_to="video/%y")
     
     def __str__(self):
