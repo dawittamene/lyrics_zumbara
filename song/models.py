e7ee4a6e627781f63b717e75fe4e1fc07fcef33a
dawittamene <dawittamene559@gmail.com>
2023-09-15 15:32:53 +0300

    commit

--- song/models.py
+++ song/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class video(models.Model):
-    discrioption=models.CharField(max_length=200)
+class Video(models.Model):
+    description=models.CharField(max_length=200)
+    video=models.FileField(upload_to="video/%y")
+    
+    def __str__(self):
+        return self.description
+    
     
